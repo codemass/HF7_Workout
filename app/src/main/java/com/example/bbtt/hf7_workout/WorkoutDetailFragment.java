c4af131cbc87f32740f9699a327e6107d23d435d
codemass <worklette@yandex.ru>
2018-08-01 15:52:23 +0300

    Изменен MainActivity. Ошибка исправлена. Нужно было импортировать app.Fragment в WorkoutDetailFragment.

--- app/src/main/java/com/example/bbtt/hf7_workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/bbtt/hf7_workout/WorkoutDetailFragment.java
@@ -2,7 +2,7 @@ package com.example.bbtt.hf7_workout;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment; //По умолчанию студия импортирует android.support.v4.app.Fragment. Но надо поменять на app.Fragment, иначе в классе MainActivity будет ошибка с приведеним типов.
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +22,7 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false); //Сообщает Android какой макет используется фрагментом (в данном случае fragment_workout_detail)
     }
 
-    public void setWorkoutId(long id) { //Метод для присваивания идентификатора. Метод используется активностью для передачи идентификатора фрагменту.
+    public void setWorkout(long id) { //Метод для присваивания идентификатора. Метод используется активностью для передачи идентификатора фрагменту.
         this.workoutId = id;
     }
 

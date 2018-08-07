706143acfee45bedf01ac7a3b7cb365e7e8fd66f
codemass <worklette@yandex.ru>
2018-08-08 02:01:35 +0300

    Добавлен слушатель в класс WorkoutListFragment.

--- app/src/main/java/com/example/bbtt/hf7_workout/WorkoutListFragment.java
+++ app/src/main/java/com/example/bbtt/hf7_workout/WorkoutListFragment.java
@@ -7,16 +7,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.app.Activity;
+import android. widget.ListView;
 
 
 public class WorkoutListFragment extends ListFragment {
 
 
-    public WorkoutListFragment() {
+    public WorkoutListFragment() {  //Этот констуктор создала сама студия.
         // Required empty public constructor
     }
 
+    //Добавление слушателя к фрагменту
+    static interface WorkoutListListener {
+        void itemClicked(long id);
+    }
 
+    private WorkoutListListener listener;
+
+    //Создание списка упражнений
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         String[] names = new String[Workout.workouts.length];
@@ -27,6 +36,19 @@ public class WorkoutListFragment extends ListFragment {
                 inflater.getContext(), android.R.layout.simple_list_item_1, names); //Получить контекст от LayoutInflater
         setListAdapter(adapter); //Связать адаптер массива со списковым представлением
         return super.onCreateView(inflater, container, savedInstanceState) ;  //Вызов метода onCreateView() суперкласса предоставляет макет по умолчанию для ListFragment
+    }
+
+    @Override
+    public void onAttach(Activity activity) { //Вызывается при присоединении фрагмента к активности
+        super.onAttach(activity);
+        this.listener = (WorkoutListListener) activity;
+    }
+
+    @Override
+    public void onListItemClick(ListView l,View v, int position, long id) {
+        if (listener != null) {
+            listener.itemClicked(id); //Сообщить слушателю о том, что на одном из вариантов ListView был сделан щелчок.
+        }
     }
 
 }

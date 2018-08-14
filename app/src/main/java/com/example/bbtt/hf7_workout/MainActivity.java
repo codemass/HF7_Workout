ea7a4d6423a8bcbee800ed20d878766326061619
codemass <worklette@yandex.ru>
2018-08-15 02:14:21 +0300

    Приложение использует разные макеты, в зависимости от того, на чем его запускают (телефон или планшет). Готово.

--- app/src/main/java/com/example/bbtt/hf7_workout/MainActivity.java
+++ app/src/main/java/com/example/bbtt/hf7_workout/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bbtt.hf7_workout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener {
 
@@ -14,15 +16,27 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-        //Здесь размещается код отображения подробной информации. Этот метод определяется в слушателе.
-        //Замена фрагмента во время выполнения происходит в виде транзакции фрагмента - набора изменений относящихся к фрагменту.
-        WorkoutDetailFragment details = new WorkoutDetailFragment();
-        FragmentTransaction ft = getFragmentManager().beginTransaction(); //Начало транзакции фрагмента.
-        details.setWorkout(id);
-        ft.replace(R.id.fragment_container,details); //Заменить фрагмент и добавить его в стек возврата.
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE); //Включить анимацию растворения и проявления фрагментов.
-        ft.commit(); //Закрепить изменения в транзакции.
-        //Теперь при выборе упражнения, справа во фрагменте с подробностями отображаются сведения о выбранных упражнениях. Кнопка назад возвражяет к предыдущему выбранному упражнению. И так до первого.
+
+        View fragmentContainer = findViewById(R.id.fragment_container); //Получить ссылку на фрейм, содержащий WorkoutDetailFragment. Он будет существовать, если приложение выполняется на устройстве с большим экраном.
+        if (fragmentContainer !=null) {
+            //Здесь размещается код отображения подробной информации. Этот метод определяется в слушателе.
+            //Замена фрагмента во время выполнения происходит в виде транзакции фрагмента - набора изменений относящихся к фрагменту.
+            //Этот код должен выполняться только в том случае, если фрейм присутсвует в макете.
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction(); //Начало транзакции фрагмента.
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container,details); //Заменить фрагмент и добавить его в стек возврата.
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE); //Включить анимацию растворения и проявления фрагментов.
+            ft.commit(); //Закрепить изменения в транзакции.
+            //Теперь при выборе упражнения, справа во фрагменте с подробностями отображаются сведения о выбранных упражнениях. Кнопка назад возвражяет к предыдущему выбранному упражнению. И так до первого.
+
+        } else {
+            //Если фрейм отсутствует, значит прилжение выполняется на устройстве с малым экраном. В этом случае следует запустить активность DetailActivity и передать ей id комплекса.
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
+
     }
 }

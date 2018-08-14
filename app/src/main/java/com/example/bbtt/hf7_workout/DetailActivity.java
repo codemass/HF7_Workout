ae4e905ee71027ee8898aec19d7d0369adb2b215
codemass <worklette@yandex.ru>
2018-08-15 01:45:07 +0300

    В DetailActivity.java добавлен код получения id комплекса из интента, запустившего активность.

--- app/src/main/java/com/example/bbtt/hf7_workout/DetailActivity.java
+++ app/src/main/java/com/example/bbtt/hf7_workout/DetailActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_WORKOUT_ID = "id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment workoutDetailFragment = (WorkoutDetailFragment) getFragmentManager().findFragmentById(R.id.detail_frag); //Получить ссылку на фрагмент
+        int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID); //Получить идентификатор комплекса, выбранного пользователем, из интента.
+        workoutDetailFragment.setWorkout(workoutId); //Передать идентификатор комплекса фрагменту.
     }
 }
